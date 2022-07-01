3b96dcaa50afef2eac125747e9a984d4ae769bb9
Denis Kravchuk <deniskrv@gmail.com>
2022-07-01 17:16:35 +0300

    сделал седьмое дз

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,15 +1,36 @@
-import {combineReducers, createStore} from "redux";
+import {applyMiddleware, combineReducers, createStore} from "redux";
 import {chatReducer} from "./reducers/chatReducer/chatReducer";
 import {messagesReducer} from "./reducers/messagesReducer/messagesReducer";
+import {createLogger} from "redux-logger/src";
+import {persistReducer, persistStore} from "redux-persist";
+import storage from 'redux-persist/lib/storage'
 
+// const logger = store => next => action => {
+//     console.log('dispatch', action)
+//     console.log('before', store.getState())
+//
+//     let result = next(action)
+//     console.log('after', store.getState())
+//
+//     return result
+// }
 
-const rootReducer=combineReducers({
+const persistConfig = {
+    key: 'root',
+    storage
+}
+
+const logger = createLogger({
+    collapsed: true,
+    diff: true
+})
+
+const rootReducer = combineReducers({
     chats: chatReducer,
     messages: messagesReducer
 })
 
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
-
-
-export const store = createStore(rootReducer)
-
+export const store = createStore(persistedReducer, applyMiddleware(logger))
+export const persist = persistStore(store)
